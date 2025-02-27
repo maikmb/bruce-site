757d4d6204e214742cab47b6aef8a2f9851ee57e
Maik Braga <maikmb@gmail.com>
2025-02-27 19:04:44 -0300

    Update site URL and base path in configuration files, and adjust image source in HTML

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://seu-usuario.github.io', // Substitua 'seu-usuario' pelo seu nome de usuário do GitHub
-  base: '/bruce-landing-page', // Substitua pelo nome do seu repositório
+  site: 'https://maikmb.github.io',
+  base: '/bruce-site',
 });
